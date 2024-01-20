14e2e2885605ac61bd0cf9ce138aa0a4de5761e7
Sylwia Rosol <sylwia.rosol.programista@gmail.com>
2024-01-20 17:01:43 +0100

    Uploading Files

--- admin/delete-article-image.php
+++ admin/delete-article-image.php
@@ -20,24 +20,33 @@ if (isset($_GET['id'])) {
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    if ($article->delete($conn)) {
+    $previous_image = $article->image_file;
 
-        Url::redirect("/admin/index.php");
+    if ($article->setImageFile($conn, null)) {
 
+        if ($previous_image) {
+            unlink("../uploads/$previous_image");
+        }
+
+        Url::redirect("/admin/edit-article-image.php?id={$article->id}");
     }
 }
 
 ?>
 <?php require '../includes/header.php'; ?>
 
-<h2>Delete article</h2>
+<h2>Delete article image</h2>
+
+<?php if ($article->image_file) : ?>
+    <img src="/uploads/<?= $article->image_file; ?>">
+<?php endif; ?>
 
 <form method="post">
 
     <p>Are you sure?</p>
 
     <button>Delete</button>
-    <a href="article.php?id=<?= $article->id; ?>">Cancel</a>
+    <a href="edit-article-image.php?id=<?= $article->id; ?>">Cancel</a>
 
 </form>
 
--- admin/includes/article-form.php
+++ admin/includes/article-form.php
@@ -20,7 +20,7 @@
 
     <div>
         <label for="published_at">Publication date and time</label>
-        <input type="datetime" name="published_at" id="published_at" value="<?= htmlspecialchars($article->published_at); ?>">
+        <input type="datetime-local" name="published_at" id="published_at" value="<?= htmlspecialchars($article->published_at); ?>">
     </div>
 
     <button>Save</button>
